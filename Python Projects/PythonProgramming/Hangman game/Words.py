145507b656bbab5d97052d7c6cd366b8f3c5964f
Patrik Eliaš <158294181+PatrikElias2@users.noreply.github.com>
2024-05-13 17:59:50 +0200

    Delete Python Projects/PythonProgramming/Hangman game directory

--- Python Projects/PythonProgramming/Hangman game/Words.py
+++
@@ -1,7 +0,0 @@
-def get_words():
-  words = ["apple", "banana", "cat", "dog", "elephant", "fun", "happy", "interesting", 
-         "jump", "kite", "laugh", "moon", "music", "nature", "ocean", "peace", "quiet", 
-         "rainbow", "read", "run", "sleep", "smile", "song", "star", "sun", "surprise", 
-         "tasty", "think", "tree", "travel", "truth", "understand", "voice", "water", 
-         "wave", "weather", "wisdom", "world", "write", "yellow", "zebra"]
-  return words
